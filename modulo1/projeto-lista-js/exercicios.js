b1b64030246b3ad83d876f52409952b1cfc59e9e
robson gavinski <ogavinski@yahoo.com.br>
2022-02-19 22:10:23 -0300

    mudança2

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -24,6 +24,7 @@ let altura1= 2
 let altura2= 2
 let largura1=4
 let largura2=4
+let soma= (altura1+altura2+largura1+largura2)
 console.log(altura1 +altura2+ largura1+largura2)
 }
 
@@ -32,6 +33,7 @@ function imprimeIdade() {
   // implemente sua lógica aqui
 const anoDeNascimento=1990
 const anoAtual=2020
+const resultado=noAtual-anoDeNascimento
 console.log(anoAtual-anoDeNascimento)
 }
 
@@ -39,27 +41,36 @@ console.log(anoAtual-anoDeNascimento)
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
 const peso=85
-const ltura=1.8
-const(altura*altura/peso)
+const altura=1.8
+const(peso/(altura*altura))
+return resultado
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
-}
+const imprimeInformacoesUsuario=prompt("meu NOME é")
+let imprimeInformacoesUsuario=prompt("tenho IDADE anos")
+const imprimeInformacoesUsuario=prompt ("meu email é EMAIL")
+const imprimeInformacoesUsuario= `Meu nome é ${NOME},tenho ${IDADE} anos,e o meu email é ${EMAIL}.`
+}console.log(imprimeInformacoesUsuario)
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
-
+const cor1 = prompt("escolha a cor 1")
+const cor2 = prompt("ecolha a cor 2")
+const cor3 = prompt("ecolha cor 3")
+const todasCores= ( cor1 ,cor2 ,cor3)
+console.log (todasCores)
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-
+const fruta = banana
+let toUpperCase("fruta")
 }
 
 // EXERCÍCIO 07
