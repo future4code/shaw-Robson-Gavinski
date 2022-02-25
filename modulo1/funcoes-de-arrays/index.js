72eea99fa6ee0bcf0d880a8c0d376f34907508e2
robson gavinski <ogavinski@yahoo.com.br>
2022-02-25 17:59:20 -0300

    inicio

---
+++ modulo1/funcoes-de-arrays/index.js
@@ -0,0 +1,61 @@
+//Exercícios de interpretação de código
+      /*  const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" }
+  ]
+  
+  const novoArrayA = usuarios.map((item, index, array) => {
+     console.log(item, index, array)
+  })*/
+
+//O que vai aparecer no console? sera impresso objeto apelido e nome de todas 
+// nova array map
+
+
+     /*const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" },
+  ]
+  
+  const novoArrayB = usuarios.map((item, index, array) => {
+     return item.nome
+  })
+  
+  console.log(novoArrayB)*/
+
+// O que vai ser imprresso no console? vai ser impresso tres arrays com nome apelido
+// de todas em coluna
+
+
+
+ /* const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" },
+  ]
+  
+  const novoArrayC = usuarios.filter((item, index, array) => {
+     return item.apelido !== "Chijo"
+  })
+  
+  console.log(novoArrayC)*/
+
+// vai imprimir 2 arrays diferentes menos a da Chijo
+
+
+  const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+  ]
+
+let nomePet = []
+
+const doguinho = pets.map((ele))
+
+
---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,28 @@
+
+ //EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+
+ 
+    const carta = comprarCarta();
+    
+    //console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    //console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+
+
+
+
+
+//1 - Imprime uma mensagem no console "Boas vindas ao jogo de Blackjack!".
+
+     console.log ("Boas vindas ao jogo de Blackjack!")
+
+
+  let pergunta = confirma ("Quer iniciar uma nova rodada")
+  if(pergunta == true){  
+
+
+  }else{     
+     console.log("O jogo acabou")
+  }
+
+
+
