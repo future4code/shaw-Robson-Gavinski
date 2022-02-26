8cc0c1413a90977b9af123bdfb0b495202b67229
robson gavinski <ogavinski@yahoo.com.br>
2022-02-26 16:05:33 -0300

    revisão

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,28 +1,49 @@
 
- //EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
-
- 
-    const carta = comprarCarta();
-    
-    //console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    //console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
-
-
-
-
-
 //1 - Imprime uma mensagem no console "Boas vindas ao jogo de Blackjack!".
 
      console.log ("Boas vindas ao jogo de Blackjack!")
 
 
-  let pergunta = confirma ("Quer iniciar uma nova rodada")
-  if(pergunta == true){  
+  if(confirm("Quer iniar uma nova rodada?")){
 
-
-  }else{     
+    }else{    
      console.log("O jogo acabou")
   }
+const primeiraCartaDoUsuario = comprarCarta()
+const segundaCartaDoUsuario = comprarCarta()
+const primeiraCartaDoComputador = comprarCarta()
+const segundaCartaDoComputador = comprarCarta()
+const somaUsuario = primeiraCartaDoUsuario.valor + segundaCartaDoUsuario.valor
+const somaComputador = primeiraCartaDoComputador + segundaCartaDoComputador.valor
+
+function cartUsuario(car1,car2,so1){
+   console.log(`Usuario - cartas: ${car1} ${car2} - ${so1}`)
+}
+function carComputador(car1,car2,so1){ 
+   console.log(`Computador - cartas: ${car1} ${car2} - ${so1}`)
+}
+
+// Invocações das funções
+ 
+cartUsuario(primeiraCartaDoUsuario.texto,segundaCartaDoUsuario.texto,somaUsuario)
+carComputador(primeiraCartaDoComputador.texto,segundaCartaDoComputador.texto,somaComputador)
+
+function quemVenceu(somaUsuario,somaComputador){ 
+   if(somaUsuario >  somaComputador && somaUsuario<= 21){ 
+      console.log("O usuario ganhou!");
+   }else if(somaComputador > somaUsuario && somaUsuario <= 21){ 
+      console.log("O computador ganhou!")
+
+ } else if(somaUsuario === somaComputador && somaUsuario,somaComputador<=21){ 
+        console.log("Empate")
+ }
+
+} 
+
+
+
+ console.log (somaUsuario,somaComputador)
+
 
 
 
